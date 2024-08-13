1a4791a0f4ce4bccb532fe63175cc687626f90f7
Dilippm <dilippm92@gmail.com>
2024-08-13 17:33:36 +0530

    sprint and subtasks model schema creates

---
+++ models/sprint.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+// Priority represents the priority level of a task.
+type Priority string
+
+const (
+	High   Priority = "High"
+	Medium Priority = "Medium"
+	Low    Priority = "Low"
+)
+
+// Sprint represents a sprint in the project management system.
+type Sprint struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	SprintName  string             `bson:"sprint_name"`
+	StartDate   time.Time          `bson:"start_date"`
+	EndDate     time.Time          `bson:"end_date"`
+	SubTasks    []primitive.ObjectID `bson:"sub_tasks"` // Array of SubTask IDs
+	Priority    Priority           `bson:"priority"`
+	UserId   primitive.ObjectID `bson:"_id,omitempty"`
+}
+
+// SubTask represents a subtask in the project management system.
+type SubTask struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	TaskName    string             `bson:"task_name"`
+	Description string             `bson:"description"`
+	StartDate   time.Time          `bson:"start_date"`
+	EndDate     time.Time          `bson:"end_date"`
+	Priority    Priority           `bson:"priority"`
+	SprintID    primitive.ObjectID `bson:"sprint_id"` // Reference to the Sprint ID
+}
