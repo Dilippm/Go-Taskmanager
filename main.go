2b568b504709e479f33f909af9b8482b82a8ad14
Dilippm <dilippm92@gmail.com>
2024-08-23 11:05:13 +0530

    cors added

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dilippm92/taskmanager/middlewares"
 	"github.com/dilippm92/taskmanager/routes"
 	"github.com/gin-gonic/gin"
+	  "github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -17,6 +18,14 @@ func main() {
 
 	}
 	router := gin.New()
+	 // CORS configuration
+	 router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5173"}, // Allow requests from this origin
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Allow these methods
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Allow these headers
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+    }))
 	router.Use(middlewares.ErrorHandler()) // Use the middleware correctly
 	router.Use(gin.Logger())
 	routes.MainRoutes(router)
